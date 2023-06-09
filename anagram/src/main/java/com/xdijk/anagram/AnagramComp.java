455fdaace2a10bef9ed427eb6d5dddb3cf774cd1
XanderDijk <xanderdijk@hotmail.com>
2023-06-09 15:34:22 +0000

    Changed naive sort implementation to more performant hashmap implementation

--- anagram/src/main/java/com/xdijk/anagram/AnagramComp.java
+++ anagram/src/main/java/com/xdijk/anagram/AnagramComp.java
@@ -1,72 +1,94 @@
 package com.xdijk.anagram;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.HashMap;
 
 /**
- * AnagramComp simple class containing static functions to determine if two input strings are anagrams of oneother
+ * AnagramComp simple class containing static functions to determine if two
+ * input strings are anagrams of oneother
  */
-public class AnagramComp 
-{
-    public static void main( String[] args ) throws Exception
-    {   
-        // Check if each argument is even or rather dividible by 2 such as that we can always form pairs and pass multiple arguments.
+public class AnagramComp {
+
+    static HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+    static HashMap<Character, Integer> map2 = new HashMap<Character, Integer>();
+
+    public static void main(String[] args) throws Exception {
+        // Check if each argument is even or rather dividible by 2 such as that we can
+        // always form pairs and pass multiple arguments.
         if (args.length % 2 == 0) {
-            for(var i = 0; i < args.length; i = i + 2) {
-                System.out.println(anagram(args[i], args[i+1]));
+            for (var i = 0; i < args.length; i = i + 2) {
+                System.out.println(anagram(args[i], args[i + 1]));
             }
         } else {
-            // (args.length + 2) - (args.length % 2) return closest in dividible by 2 greater than arg.length
-            throw new Exception(String.format("Invalid number of arguments, amount provided: %s expected: %s", args.length, (args.length + 2) - (args.length % 2)));
+            // (args.length + 2) - (args.length % 2) return closest in dividible by 2
+            // greater than arg.length
+            throw new Exception(String.format("Invalid number of arguments, amount provided: %s expected: %s",
+                    args.length, (args.length + 2) - (args.length % 2)));
         }
     }
 
     /*
-     * There is argument to be made that this being a even protected function is a code-smell since it is only used in Main()
+     * There is argument to be made that this being a even protected function is a
+     * code-smell since it is only used in Main()
      */
     protected static boolean anagram(String input, String input2) {
         if (input != null && !input.trim().isEmpty() || input2 != null && !input2.trim().isEmpty()) {
-            // To lowercase() to ensure that sorting will be the same between words and sentences
+            // To lowercase() to ensure that sorting will be the same between words and
+            // sentences
             input = input.toLowerCase();
             input2 = input2.toLowerCase();
 
             input = stripSpecialCharacters(input);
             input2 = stripSpecialCharacters(input2);
 
-            // Since I am considering introducing punctiation in an anagram as valid it has had to be sanitized before an early exit.
-            if(input.length() != input2.length()) {
+            // Since I am considering introducing punctiation in an anagram as valid it has
+            // had to be sanitized before an early exit.
+            if (input.length() != input2.length()) {
                 return false;
             }
 
-            var list = stringToCharList(input);
-            var list2 = stringToCharList(input2);
+            HashMap<Character, Integer> map = readStringIntoMap(input);
+            HashMap<Character, Integer> map2 = readStringIntoMap(input2);
 
-            //sort so that they are in natural order
-            list = list.stream().sorted().collect(Collectors.toList());
-            list2 = list2.stream().sorted().collect(Collectors.toList());
-            return list.equals(list2);
+            return map.equals(map2);
         }
 
         return false;
     }
 
     /*
-     * Since it is just a small technical test this is all written in a single class.
-     * If util functions like this are common throughout an applicationt his would be extracted into a utils package.
-     * There is argument to be made that this being a even protected function is a code-smell since it is only used in anagram()
-     * However going against convention I believe that a validation function like this should be tested.
+     * reads string into HashMap<Character, Integer> where character is the key and
+     * Integer is the amount of occurences of the character within the given string.
+     */
+    private static HashMap<Character, Integer> readStringIntoMap(String input) {
+        var map = new HashMap<Character, Integer>();
+        var charArr = input.toCharArray();
+        for (char c : charArr) {
+            if (map.get(c) == null) {
+                map.put(c, 1);
+            } else {
+                var val = map.get(c);
+                map.put(c, ++val);
+            }
+        }
+
+        return map;
+    }
+
+    /*
+     * Since it is just a small technical test this is all written in a single
+     * class.
+     * If util functions like this are common throughout an applicationt his would
+     * be extracted into a utils package.
+     * There is argument to be made that this being a even protected function is a
+     * code-smell since it is only used in anagram()
+     * However going against convention I believe that a validation function like
+     * this should be tested.
      */
     protected static String stripSpecialCharacters(String input) {
         // [\\P{LD}+^] is a regex that matches any unicode non-word character or digit
-        // Instead of a common a-zA-Z0-9 based match which may not deal with non-roman characters
+        // Instead of a common a-zA-Z0-9 based match which may not deal with non-roman
+        // characters
         var output = input.replaceAll("[\\P{LD}+^]", "");
         return output;
-    }
-
-    private static List<Character> stringToCharList(String input) {
-        var list = input.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
-        return list;
     }
 }
--- anagram/src/test/java/com/xdijk/anagram/AnagramCompTest.java
+++ anagram/src/test/java/com/xdijk/anagram/AnagramCompTest.java
@@ -6,8 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Arrays;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -16,93 +14,89 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 /**
  * Unit test for AnagramComp.
  */
-public class AnagramCompTest 
-{
+public class AnagramCompTest {
 
     @Test
-    public void MainTest()
-    {   
-        assertDoesNotThrow(() -> AnagramComp.main(new String[]{"New York Times", "monkeys write"}));
-        Exception exception = assertThrows(Exception.class, () -> AnagramComp.main(new String[]{"New York Times"}));
-        
+    public void MainTest() {
+        assertDoesNotThrow(() -> AnagramComp.main(new String[] { "New York Times", "monkeys write" }));
+        Exception exception = assertThrows(Exception.class, () -> AnagramComp.main(new String[] { "New York Times" }));
+
         var msg = exception.getMessage();
         assertTrue(msg.contains("Invalid number of arguments"));
     }
 
     /**
-     * Non exhaustive list of anagrams attempting to cover a range of input including unicode characters and special characters.
+     * Non exhaustive list of anagrams attempting to cover a range of input
+     * including unicode characters and special characters.
      */
     @ParameterizedTest
     @CsvSource({
-    "New York Times, monkeys write",
-    "Church of Scientology, rich-chosen goofy cult",
-    "McDonald's restaurants, Uncle Sam's standard rot",
-    "evil, vile",
-    "William Shakespeare, I am a weakish speller",
-    "IM FEARLESS, LE SSERAFIM",
-    "Anagrams, Ars magna",
-    "가나, 나가", 
-    "국왕, 왕국"
+            "New York Times, monkeys write",
+            "Church of Scientology, rich-chosen goofy cult",
+            "McDonald's restaurants, Uncle Sam's standard rot",
+            "evil, vile",
+            "William Shakespeare, I am a weakish speller",
+            "IM FEARLESS, LE SSERAFIM",
+            "Anagrams, Ars magna",
+            "가나, 나가",
+            "국왕, 왕국"
     })
-    public void AnagramCompTestTrue(String input1, String input2)
-    {
-        assertTrue( AnagramComp.anagram(input1, input2) );
+    public void AnagramCompTestTrue(String input1, String input2) {
+        assertTrue(AnagramComp.anagram(input1, input2));
     }
 
-
     /**
-     * Non exhaustive list of non-anagrams attempting to cover a range of input including unicode characters and special characters.
+     * Non exhaustive list of non-anagrams attempting to cover a range of input
+     * including unicode characters and special characters.
      */
     @ParameterizedTest
     @CsvSource({
-    "New York Times, monkey write",
-    "Church of Scientology, rich-chosen loafy cult",
-    "McDonald's restaurants, Uncle Sam's standard dance",
-    "evil, great",
-    "William Shakes, I am a weakish speller",
-    "Anagrams, magna",
-    "가나, 가\uC655", 
-    "국왕, 왕\uAC00"
+            "New York Times, monkey write",
+            "Church of Scientology, rich-chosen loafy cult",
+            "McDonald's restaurants, Uncle Sam's standard dance",
+            "evil, great",
+            "William Shakes, I am a weakish speller",
+            "Anagrams, magna",
+            "가나, 가\uC655",
+            "국왕, 왕\uAC00"
     })
-    public void AnagramCompTestFalse(String input1, String input2)
-    {
+    public void AnagramCompTestFalse(String input1, String input2) {
         assertFalse(AnagramComp.anagram(input1, input2));
     }
 
     @ParameterizedTest
     @NullAndEmptySource
-    public void AnagramCompTestCheckNullAndEmpty(String input)
-    {
+    public void AnagramCompTestCheckNullAndEmpty(String input) {
         assertFalse(AnagramComp.anagram(input, input));
     }
-
 
     /**
      * 
      */
     @ParameterizedTest
-     @CsvSource({
-    "New York Times', NewYorkTimes",
-    "Church@Scientology, ChurchScientology",
-    "McDonald/restaurants, McDonaldrestaurants",
-    "evil\u3002great, evilgreat",
+    @CsvSource({
+            "New York Times', NewYorkTimes",
+            "Church@Scientology, ChurchScientology",
+            "McDonald/restaurants, McDonaldrestaurants",
+            "evil\u3002great, evilgreat",
     })
-    public void stripSpecialCharactersTest(String input, String expected)
-    {
+    public void stripSpecialCharactersTest(String input, String expected) {
         assertEquals(expected, AnagramComp.stripSpecialCharacters(input));
     }
 
-
     /*
-     * @CsvSource doesn't play nice with some special characters and breaks the Parameterized input
+     * @CsvSource doesn't play nice with some special characters and breaks the
+     * Parameterized input
      * Therefor it is split into two seperate tests.
      */
     @Test
-    public void stripSpecialCharactersTest2()
-    {
+    public void stripSpecialCharactersTest2() {
         assertEquals("Anagramsmagna", AnagramComp.stripSpecialCharacters("Anagrams \n magna"));
-        assertEquals("\u6BDB\u7403", AnagramComp.stripSpecialCharacters("\u300C\u6BDB\u7403\u300D")); //Chinese 「毛球」to 毛球
-        assertEquals("\uADF8\uB807\uAD6C\uB098", AnagramComp.stripSpecialCharacters("\uADF8\uB807\uAD6C\uB098~")); //Korean: 그렇구나~ to 그렇구나
 
+        assertEquals("\u6BDB\u7403", 
+            AnagramComp.stripSpecialCharacters("\u300C\u6BDB\u7403\u300D")); // Chinese 「毛球」to 毛球
+
+        assertEquals("\uADF8\uB807\uAD6C\uB098",
+            AnagramComp.stripSpecialCharacters("\uADF8\uB807\uAD6C\uB098~")); // Korean: 그렇구나~ to 그렇구나
     }
 }
